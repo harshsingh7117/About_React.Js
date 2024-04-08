ef204f05c09bacc77dad23ec37b7e74a627c6400
harshsingh7117 <harshsingh7117@gmail.com>
2024-04-09 01:17:14 +0530

    .

---
+++ 01reactbasic/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./One"
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>This is Main Headind(h1).</h1>
+   <Chai/>
+   <p>This is Paragraph.</p>
+   </>
+  )
+}
+
+export default App
---
+++ 01reactbasic/src/One.jsx
@@ -0,0 +1,11 @@
+
+
+
+function Chai(){
+    return(
+        <h3>Another Heading h3</h3>
+    )
+}
+
+
+export default Chai
