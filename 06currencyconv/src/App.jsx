5726480c31c42ce0e68c6f698c6900f2c7937949
harshsingh7117 <harshsingh7117@gmail.com>
2024-05-12 02:37:22 +0530

    .change

--- 06currencyconv/src/App.jsx
+++ 06currencyconv/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { InputBox } from "./Components";
-import useCurrencyInfo from './hooks/useCurrencyInfo';
-import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
   const [amount, setAmount] = useState(0);
@@ -28,12 +27,12 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://images.pexels.com/photos/20755699/pexels-photo-20755699/free-photo-of-a-waterfall-in-the-mountains-with-a-green-valley-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load')`,
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}
     >
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-          <from
+          <form
             onSubmit={(e) => {
               e.preventDefault();
               convert();
@@ -52,12 +51,7 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2
-                  -translate-x-1/2
-                  -translate=y-1/2 border-2
-                  border-white rounded-md
-                  bg-blue-600 text-white px-2
-                  py-0.5"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                 onClick={swap}
               >
                 swap
@@ -68,21 +62,18 @@ function App() {
                 label="To"
                 amount={convertedAmount}
                 currencyOptions={options}
-                onCurrencyChange={(currency) => {
-                  setTo(currency);
-                }}
+                onCurrencyChange={(currency) => setTo(currency)}
                 selectCurrency={from}
                 amountDisable
               />
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white px-4
-            py-3 rounded-lg"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
             >
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
-          </from>
+          </form>
         </div>
       </div>
     </div>
